7bb83380d2320b7ddeec8830fa11402097f6bde1
Jamal Frank <jamaljfrank@gmail.com>
2019-10-06 01:13:08 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,46 +5,5 @@ require_relative '../lib/concerns/findable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
-class Genre
-
-  attr_accessor :name
-  extend Concerns::Findable
-  
-  @@all = []
-
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    song = Genre.new(name)
-    song.save
-    return song
-  end
-
-  def songs 
-    @songs
-  end
-
-  def artists
-    artists = @songs.collect {|song| song.artist}
-    artists.uniq 
-  end
-
-
-end  
+require_relative '../lib/genre.rb'
 
